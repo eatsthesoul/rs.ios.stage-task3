b2fab953f4bdf15237c0ee3b2044307a28ade04e
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2021-05-23 19:52:39 +0300

    feat: add solution for exercise-2

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,25 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    
+    int counter = 0;
+    
+    for(int i = 0; i < ratingArray.count; i++) {
+        for(int j = i + 1; j < ratingArray.count; j++) {
+            for(int k = j + 1; k < ratingArray.count; k++) {
+                
+                if (ratingArray[i].intValue < ratingArray[j].intValue && ratingArray[j].intValue < ratingArray[k].intValue) {
+                    counter++;
+                }
+                
+                if (ratingArray[i].intValue > ratingArray[j].intValue && ratingArray[j].intValue > ratingArray[k].intValue) {
+                    counter++;
+                }
+            }
+        }
+    }
+    
+    return counter;
 }
 
 @end
