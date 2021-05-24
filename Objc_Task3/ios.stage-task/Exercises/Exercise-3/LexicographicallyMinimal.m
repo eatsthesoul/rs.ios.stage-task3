59247c37963e7e7e4090cf44e4b0572559fefce7
Evgeniy Petlitskiy <46849100+eatsthesoul@users.noreply.github.com>
2021-05-24 09:32:12 +0300

    feat: add solution for the exercise-3

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,30 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    
+    NSMutableString *commonString = [[NSMutableString alloc] initWithString:@""];
+    NSMutableString *mutableString1 = [[NSMutableString alloc] initWithString:string1];
+    NSMutableString *mutableString2 = [[NSMutableString alloc] initWithString:string2];
+    
+    //add chars to the common string until one of the mutable strings become empty
+    while (mutableString1.length != 0 && mutableString2.length != 0) {
+        
+        if (mutableString1.UTF8String[0] <= mutableString2.UTF8String[0]) {
+            
+            [commonString appendFormat:@"%c", [mutableString1 characterAtIndex:0]];
+            [mutableString1 deleteCharactersInRange: NSMakeRange(0, 1)];
+            
+        } else {
+            
+            [commonString appendFormat:@"%c", [mutableString2 characterAtIndex:0]];
+            [mutableString2 deleteCharactersInRange: NSMakeRange(0, 1)];
+        }
+    }
+    
+    //add the remaining string to the common string
+    mutableString1.length == 0 ? [commonString appendString:mutableString2] : [commonString appendString:mutableString1];
+    
+    return commonString;
 }
 
 @end
